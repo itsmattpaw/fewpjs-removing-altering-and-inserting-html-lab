00582b56cbdc4d1ac4af2b04b7720136c230d85d
itsmattpaw <itsmattpaw@gmail.com>
2022-06-28 14:16:21 -0400

    commit tree 914a6a01e2b033363a1ef5bb37087d5e1108fce0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let x = document.getElementById('main')
+x.remove()
+
+let body = document.querySelector('body')
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.textContent = "Matt is the champion"
